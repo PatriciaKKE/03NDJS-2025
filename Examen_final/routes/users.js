1962e693c7a4cb96300e9058d5ff2ceee34954a1
patricia <kamleuemmerencia@gmail.com>
2025-04-18 01:41:27 +0200

    users.js

---
+++ Examen_final/routes/users.js
@@ -0,0 +1,10 @@
+t express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+const authMiddleware = require('../middleware/auth');
+
+router.get('/me', /*authMiddleware,*/ userController.getMe);
+router.get('/', /*authMiddleware,*/ userController.getAllUsers);
+router.delete('/:id', /*authMiddleware,*/ userController.deleteUser);
+
+module.exports = router;
