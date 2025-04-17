f040e7fca4e077174a20bcc66a7d3e83d054dbdb
patricia <kamleuemmerencia@gmail.com>
2025-04-18 01:46:16 +0200

    auth.js

--- Examen_final/routes/auth.js
+++ Examen_final/routes/auth.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router(); 
-const authController = require('../controllers/authController'); 
+const authControllers = require('../controllers/authControllers'); 
 
-router.post('/register', authController.register); 
-router.post('/login', authController.login); 
+router.post('/register', authControllers.register); 
+router.post('/login', authControllers.login); 
 
 module.exports = router; 
