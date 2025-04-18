bd1391a65b6f2e166f6b17821158fca48b9a0cc2
patricia <kamleuemmerencia@gmail.com>
2025-04-18 10:40:12 +0200

    auth.js

--- Examen_final/middleware/auth.js
+++ Examen_final/middleware/auth.js
@@ -12,13 +12,24 @@ const authMiddleware = (req, res, next) => {
     return res.status(401).json({ message: 'Token inexistant' });
   }
 
-  jwt.verify(token, 'fdsFDu2H9L4Apl6C3CD6BTvSoPJnm7vHuzTOlxZWEDE=', (err, user) => {
+  jwt.verify(token, 'fdsFDu2H9L4Apl6C3CD6BTvSoPJnm7vHuzTOlxZWEDE=', (err, decoded) => { 
     if (err) {
       console.log('Token invalide:', err.message);
       return res.status(403).json({ message: 'Token invalide' });
     }
+
+    const userId = decoded.id; 
+
+
+    const user = User.getById(userId);
+
+    if (!user) {
+      console.log('Utilisateur non trouvé');
+      return res.status(401).json({ message: 'Utilisateur non trouvé' });
+    }
+
     console.log('Utilisateur vérifié:', user);
-    req.user = user;
+    req.user = user; 
     next();
   });
 };
