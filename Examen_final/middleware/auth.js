0e9a0919a8cbc553425722be6262aa80ee87ac40
patricia <kamleuemmerencia@gmail.com>
2025-04-18 09:32:00 +0200

    auth.js

--- Examen_final/middleware/auth.js
+++ Examen_final/middleware/auth.js
@@ -2,18 +2,22 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const authMiddleware = (req, res, next) => {
+  console.log('Début du middleware d\'authentification');
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(' ')[1];
+  console.log('Token:', token);
 
   if (!token) {
-    return res.status(401).json({ message: 'Token manquant' });
+    console.log('Token manquant');
+    return res.status(401).json({ message: 'Token inexistant' });
   }
 
   jwt.verify(token, 'xUCgqueIYDfLEDsmOt1ujaXnuP1zSru2xsOocw87dHI=', (err, user) => {
     if (err) {
+      console.log('Token invalide:', err.message);
       return res.status(403).json({ message: 'Token invalide' });
     }
-
+    console.log('Utilisateur vérifié:', user);
     req.user = user;
     next();
   });
